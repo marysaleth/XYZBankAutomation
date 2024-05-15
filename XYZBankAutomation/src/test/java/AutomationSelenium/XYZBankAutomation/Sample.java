8bf2ef8a17075d63399baae85255ba64b982961a
Saleth Mary <marysaleth0@gmail.com>
2024-05-15 15:00:53 +0530

    first commit

---
+++ XYZBankAutomation/src/test/java/AutomationSelenium/XYZBankAutomation/Sample.java
@@ -0,0 +1,21 @@
+package AutomationSelenium.XYZBankAutomation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+	
+		
+	}
+
+}
